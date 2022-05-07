d6e8d07b95f80ee959bd8934721908e65efad695
Oleh Topchak <oleh.topchak@gmail.com>
2022-05-07 21:07:52 +0300

    Removed logs and cleaned the code

--- index.ts
+++ index.ts
@@ -23,11 +23,8 @@ function getPlaylists(screenKey: string) {
       };
 
       const req = https.request(reqOptions, (res: IncomingMessage) => {
-        console.log(`statusCode: ${ res.statusCode }`);
-
         res.on('data', (d: string) => {
           const playlist: PlaylistResponse = JSON.parse(d);
-          console.log('DATa', playlist);
           return resolve(playlist);
         });
       });
@@ -43,37 +40,6 @@ function getPlaylists(screenKey: string) {
   });
 }
 
-function getMedia(imageKey: string) {
-  return new Promise((resolve, reject) => {
-    try {
-      const reqImageOptions = {
-        hostname: 'test.onsignage.com',
-        port: 443,
-        path: `/PlayerBackend/creative/get/${ imageKey }`,
-        method: 'GET',
-      };
-
-      const reqImage = https.request(reqImageOptions, (resImage: IncomingMessage) => {
-        resImage.on('data', (dImage) => {
-          console.log('DATA', dImage);
-          const image = JSON.stringify(dImage);
-          reqImage.end();
-
-          return resolve(image);
-        });
-      });
-
-      reqImage.on('error', (error: Error) => {
-        console.error(error);
-      });
-
-    } catch (e) {
-      console.error(e);
-      reject(e);
-    }
-  });
-}
-
 app.use(cors());
 
 app.get('/key', async (req: Request, res: Response) => {
@@ -83,16 +49,8 @@ app.get('/key', async (req: Request, res: Response) => {
   res.send(playlists);
 });
 
-// app.get('/media', async (req: Request, res: Response) => {
-//   const defaultMediaKey = 'b1f1b49b-46b8-49ef-8177-309d28128bf7.jpg';
-//   const screenKey = req.params[ 'mediaKey' ] || defaultMediaKey;
-//   const playlists = await getMedia(screenKey);
-//   res.send('http://teststatic.onsignage.com/test/media/b/1/f/1/b1f1b49b-46b8-49ef-8177-309d28128bf7.jpg');
-// });
-
 app.get('/media', proxy('test.onsignage.com', {
   proxyReqPathResolver: function (req: Request) {
-    console.log(req);
     return '/PlayerBackend/creative/get/' + req.query[ 'mediaKey' ];
   }
 }));
